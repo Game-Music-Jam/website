bd2f00dcb3fedd98aab74ce9b10010f31311b935
link5669 <milesacq@icloud.com>
2022-01-07 21:44:21 -0500

    refactor and menu continuity

--- js/main.js
+++ js/main.js
@@ -13,3 +13,53 @@
 	fullHeight();
 
 })(jQuery);
+
+//B722B2DF3D9D1CC7C19BBC81D410AD65D9FF
+function sendMail() {
+	var name = document.getElementById("exampleFormControlInput1").value;
+	var email = document.getElementById("exampleFormControlInput2").value;
+	console.log(email);
+	var body = document.getElementById("exampleFormControlTextarea1").value;
+	Email.send({
+		Host : "smtp.elasticemail.com",
+		Username : "acqmiles@gmail.com",
+		Password : "B722B2DF3D9D1CC7C19BBC81D410AD65D9FF",
+		To : 'admin@gamemusicjam.org',
+		From : "acqmiles@gmail.com",
+		Subject : "GM JAM CONTACT FORM",
+		Body : "FROM: " + name + " " + email + " MESSAGE: " + body
+	}).then(
+	  message => alert(message)
+	);
+
+}
+function show() {
+	if (document.getElementById("jam1").style.visibility == "visible") {
+		document.getElementById("jam1").style.visibility = "hidden";
+		document.getElementById("newMenu").style.visibility = "hidden";
+	} else {
+		document.getElementById("jam1").style.visibility = "visible";
+	}
+}
+function show2020() {
+	document.getElementById("newMenu").style.visibility = "visible";
+	document.getElementById("menu1").textContent = "GM Jam '20";
+	document.getElementById("menu1").href = "gm20.html";
+	document.getElementById("menu2").style.padding = "0px"
+}
+function show2021() {
+	document.getElementById("newMenu").style.visibility = "visible";
+	document.getElementById("menu1").textContent = "GM Jam '21";
+	document.getElementById("menu1").href = "gm21.html";
+	document.getElementById("menu2").style.padding = "0px"
+}
+function show2022() {
+	document.getElementById("newMenu").style.visibility = "hidden";
+}
+function showUpcoming() {
+	document.getElementById("newMenu").style.visibility = "hidden";
+}
+var md = new MobileDetect(window.navigator.userAgent);
+if (md.mobile() || md.tablet()) {
+	document.location.href = 'mobile-home.html';
+}

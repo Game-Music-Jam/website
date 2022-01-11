0ff8245a1ca3e825317d1912bc1e65cbd122a784
link5669 <milesacq@icloud.com>
2022-01-10 23:02:17 -0500

    .

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-sm navbar-dark fixed-top py-3" id="mainNav"  style="background-color: #7D4D8F;filter: drop-shadow(0 0 0.75rem black);">
+            <div class="container-fluid ">
+              <a class="nav-item ms-auto" ><a class="nav-link" style="font-family: Montserrat; color:white;" href="index.html"><b>Game/Music&nbsp;Jam</b></a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <div class="container">
+                            <div class="row">
+                                <div class="dropdown" style="font-family: Montserrat; color:white;">
+                                    <div class="dropdown"> 
+                                        <a href="#!"style="color:white;  "class="nav-link" id="dropdownMenuButton" onclick="show()" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Events</a>
+                                            <div style="background-color: #7D4D8F; color:white" class="dropdown-menu visi" aria-labelledby="dropdownMenuButton" id="jam1" >
+                                                <a class="dropdown-item" style="color:white" onClick="showUpcoming()" href="#">Upcoming Events</a>
+                                                <a class="dropdown-item" style="color:white" onClick="show2022()" href="#">2022</a>
+                                                <a class="dropdown-item" style="color:white" onClick="show2021()" href="#">2021</a>
+                                                <a class="dropdown-item" style="color:white" onClick="show2020()" href="#">2020</a>
+                                            </div>
+                                    </div>
+                                </div> 
+                                <div id="topmenu" class="dropdown" style="padding-left: 0px; padding-right: 0px; font-family: Montserrat; background-color:#7D4D8F; max-width: 0px; position: relative; right:313px">
+                                    <div class="dropdown" style="padding-left: 70px; padding-top: 39px; max-width: 0px; cobackground-colorlor:#7D4D8F">
+                                        <div style="background-color:#7D4D8F" class="dropdown-menu twvisi" aria-labelledby="dropdownMenuButton" id="newMenu">
+                                            <a class="dropdown-item" style="color:white" id="menu1"></a>
+                                            <a class="dropdown-item" style="color:white" id="menu2"></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="nav-item" style="z-index: 1000;">
+                        <a class="nav-link active" aria-current="page" style="color:white; z-index: 1000;" href="about.html">Team</a>
+                      </li>		
+                      <li class="nav-item" style="padding-right: 5px">
+                        <a class="nav-link active" aria-current="page" style="color:white" href="contact.html">Contact</a>
+                      </li>	
+                </ul>		  
+              </div>
+            </div>
+          </nav>
